76923d094e38715a863091a7f237700f262723a9
antonborod1n <antonborodin86@gmail.com>
2023-09-05 10:55:30 +0300

    added desctop

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+import Swiper from 'swiper';
+
 window.addEventListener('DOMContentLoaded', function () {
   const menuBurger = document.querySelector('.menu__burger');
   const menuBox = document.querySelector('.menu__box');
@@ -7,4 +9,26 @@ window.addEventListener('DOMContentLoaded', function () {
   menuBurger.addEventListener('click', function () {
     menuBox.classList.toggle('active');
   });
+});
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
